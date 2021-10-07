a0f600a543c868a8c62f8d59b0e6bf3e57896875
Vildan Akbulut <88880914+Vildan1@users.noreply.github.com>
2021-10-07 12:33:30 +0300

    ucuncu gonderme

---
+++ ui/angular10/src/app/shared.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+   readonly APIUrl="http://localhost:64403/api";
+   readonly PhotoUrl="http://localhost:64403/Photos";
+  constructor(private http:HttpClient) { }
+
+  getDepList(): Observable<any[]>{
+    return this.http.get<any>(this.APIUrl+ '/department')
+  }
+
+  addDepartment(val:any){
+    return this.http.post(this.APIUrl+ '/department',val)
+  }
+  updateDepartment(val:any){
+    return this.http.put(this.APIUrl+ '/department',val)
+  }
+  deleteDepartment(val:any){
+    return this.http.delete(this.APIUrl+ '/department',val)
+  }
+  
+  getEmpList(): Observable<any[]>{
+    return this.http.get<any>(this.APIUrl+ '/employee')
+  }
+
+  addEmployee(val:any){
+    return this.http.post(this.APIUrl+ '/employee',val)
+  }
+  updateEmployee(val:any){
+    return this.http.put(this.APIUrl+ '/employee',val)
+  }
+  deleteEmployee(val:any){
+    return this.http.delete(this.APIUrl+ '/employee',val)
+  }
+  UploadPhoto(val:any){
+    return this.http.post(this.APIUrl+ '/employee/SaveFile',val)
+  }
+  getAllDeparmentNames():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl+ '/employee/GetAllDeaprmentName' )
+  }
+  
+}
